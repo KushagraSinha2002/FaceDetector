6147810adcf7c23474f846989da0973cd66920b7
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-09-04 03:27:22 +0530

    Detect face and surround with rect with random colour
    -Add comments
    -Convert img to grayscale
    -Use detectMultiScale on img to
      return coordinates of the face in
      the img. coordinates eg;-(X , Y, (
      X+W), (Y+H))
    -Run for loop for X,Y,W,H on
      coordinates obtained to draw
      rectangles on face(s) in the img.

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,10 +1,30 @@
 import cv2
+from random import randrange
 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Load some pre-trained data on face frontals from opencv (haar cascade algorithims)
+trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+# Choose an image to stetct faces in
 img = cv2.imread('rdj1.jpg')
 
+# Must convert to greyscale
+greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect Faces, detectMultiscale returns coordinates face in image
+face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
+
+# Draw rectangles around the face
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x + w , y + h) , (randrange(128,256), randrange(128,256), randrange(128,256)), 10)
+#cv2.rectangle(img, (x,y), (x + w , y + h) , (0, 255, 0), 2)
+
+
+print(face_coordinates)
+#[[240  91 272 272]]
+# x   y   w   h
+
+# Open the image
 cv2.imshow('Sucess',img)
 cv2.waitKey()
 
-print("Code")
+print("Code Completed")

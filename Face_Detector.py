f62a227e1097cbd83f19f768f67396fda8d860b1
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-09-05 13:29:27 +0530

    Run project through default camera
    -Add support to run face detection through the default camera

--- Face_Detector.py
+++ Face_Detector.py
@@ -4,27 +4,26 @@ from random import randrange
 # Load some pre-trained data on face frontals from opencv (haar cascade algorithims)
 trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Choose an image to stetct faces in
-img = cv2.imread('rdj1.jpg')
+webcam = cv2.VideoCapture(0)
 
-# Must convert to greyscale
-greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
 
-# Detect Faces, detectMultiscale returns coordinates face in image
-face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
+    successful_frame_read, frame = webcam.read()
 
-# Draw rectangles around the face
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x,y), (x + w , y + h) , (randrange(128,256), randrange(128,256), randrange(128,256)), 10)
-#cv2.rectangle(img, (x,y), (x + w , y + h) , (0, 255, 0), 2)
+    # Must convert to greyscale
+    greyscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   
 
+    # Detect Faces, detectMultiscale returns coordinates face in image/video
+    face_coordinates = trained_face_data.detectMultiScale(greyscaled_img)
 
-print(face_coordinates)
-#[[240  91 272 272]]
-# x   y   w   h
+    # Draw rectangles around the face
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x,y), (x + w , y + h) , (randrange(128,256), randrange(128,256), randrange(128,256)), 10)
+    #cv2.rectangle(img, (x,y), (x + w , y + h) , (0, 255, 0), 2)
 
-# Open the image
-cv2.imshow('Sucess',img)
-cv2.waitKey()
+    cv2.imshow('Sucess',frame)
+    key = cv2.waitKey(1)
 
-print("Code Completed")
+    # Stop if pressed q or Q
+    if key == 113 or key == 81:
+        break
